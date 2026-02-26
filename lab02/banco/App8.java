4737b80321cc8f0d2e8421ba42ab1194066607d2
aaronnur <145123249+aaronnur@users.noreply.github.com>
2026-02-26 01:41:58 +0000

    finalizado

---
+++ lab02/banco/App8.java
@@ -0,0 +1,13 @@
+public class App8 {
+    public static void main(String[] args) throws Exception {
+        Banco meuBanco = new Banco("Java");
+        meuBanco.adicionar(new ContaInfinita(1111, 50));
+        meuBanco.adicionar(new ContaBancaria(2222, 99));
+        meuBanco.adicionar(new ContaBancaria(3333, 1000));
+        meuBanco.adicionar(new ContaInfinita(4444, 999));
+        meuBanco.adicionar(new ContaEspecial(7777, 800, 200));
+        meuBanco.aumentarLimiteEspeciais(200);
+        meuBanco.tentarSacar(1100);
+        meuBanco.mostrar();
+    }
+}
